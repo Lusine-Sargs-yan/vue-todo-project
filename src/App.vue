0554907e7b89e0aec538b54c90eba26013be0e75
Lusine-Sargs-yan <sargsyanlq@gmail.com>
2022-03-28 07:44:47 +0400

    add vue project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <the-header title="RememberMe"></the-header>
+    <the-resource></the-resource>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/layouts/TheHeader.vue";
+import TheResource from "./components/learning-resources/TheResource.vue";
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader,
+    TheResource,
+  },
+
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,500;1,700;1,900&display=swap');
+* {
+  box-sizing: border-box;
+}
+html, body {
+  font-family: 'Roboto', sans-serif;
+  margin: 0;
+}
+/*#app {*/
+/*  font-family: Avenir, Helvetica, Arial, sans-serif;*/
+/*  -webkit-font-smoothing: antialiased;*/
+/*  -moz-osx-font-smoothing: grayscale;*/
+/*  text-align: center;*/
+/*  color: #2c3e50;*/
+/*  margin-top: 60px;*/
+/*}*/
+</style>
---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: ['mode'],
+}
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem;
+  font-family: inherit;
+  background-color: #3a0061;
+  border: 1px solid #3a0061;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #270041;
+  border-color: #270041;
+}
+.flat {
+  background-color: transparent;
+  color: #3a0061;
+}
+.flat:hover,
+flat:active {
+  background-color: #edd2ff;
+}
+</style>
